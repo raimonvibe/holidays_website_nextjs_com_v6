18a138e3a1ca6fffbbc07c14c4b39558f21139c5
raimonvibe <robertjanstefan@gmail.com>
2024-10-28 08:16:37 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,8 +55,6 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <title>Home raimonvibe</title>
         <Script type="text/javascript" charSet="UTF-8" src="//cdn.cookie-script.com/s/bc6138fbc116438ba4c0b26d11e59b5f.js" />
-        <link rel="stylesheet" href="css/normalize.css" />
-        <link rel="stylesheet" href="css/styles.css" />
         <link rel="icon" href="/favicon-new.ico" />
         <link rel="canonical" href="https://raimonvibe.com/" />
         <link rel="alternate" hrefLang="en" href="https://raimonvibe.com" />
