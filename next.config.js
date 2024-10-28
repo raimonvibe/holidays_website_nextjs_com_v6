ab2a4d927373197695dbab4a20b2ebce0394871f
raimonvibe <robertjanstefan@gmail.com>
2024-10-28 08:40:54 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,16 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'ih1.redbubble.net',
-      },
-    ],
+    domains: ['ih1.redbubble.net'], // Voeg hier je afbeeldingsdomeinen toe
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Gaat voor alle routes gelden
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' https://www.youtube.com; frame-src https://www.youtube.com; img-src 'self' ih1.redbubble.net data:; style-src 'self' 'unsafe-inline';", // Pas aan volgens je behoeften
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
