1c30ed927debf2357f4bd6ac651d61c1c9926fea
raimonvibe <robertjanstefan@gmail.com>
2024-12-17 06:23:26 +0100

    Update page.tsx

--- src/app/Services/page.tsx
+++ src/app/Services/page.tsx
@@ -25,51 +25,9 @@ export default function Diensten() {
   return (
     <div>
       <main>
-        <section className="middle-container">
-          <Image
-            className="common-properties common-properties-right"
-            src={developer}
-            alt="Illustration of a software developer working on a computer."
-            width={500}
-            height={300}
-            priority
-          />
-          <article>
-            <div>
-              <h1>Software Engineering Solutions</h1>
-              <p>
-With my experience in software engineering, I've been creating contemporary, user-friendly websites using established techniques. I also provide code documentation services, ensuring clarity and helping aspiring developers understand the process better. Being active on Upwork and Fiverr, I'm dedicated to delivering high-quality work with precision and attention to detail.
-              </p>
-              <div className="spacing"></div>
-              <span>
-                <p>Check out my Freelance profile at:</p>
-                <strong>
-                  <a
-                    className="Social"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://www.upwork.com/freelancers/~01db073e93aefd8fac"
-                  >
-                    Upwork
-                  </a>
-                </strong>
-                <br></br>
-                <div className="spacing"></div>
-                <strong>
-                  <a
-                    className="Social"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://nl.fiverr.com/raimonvibe"
-                  >
-                    Fiverr
-                  </a>
-                </strong>
-              </span>
-              <div className="spacing3"></div>
-            </div>
-          </article>
-        </section>
+     
+   
+    
         <section className="middle-container2">
           <div>
             <Image
